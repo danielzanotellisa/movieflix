067d0a0b44b13dcfa241440d53c5addea22a9658
Daniel Zanotelli <danielzanotelli12@gmail.com>
2024-11-21 13:25:16 -0300

    chore: first commit

---
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package com.movieflix.controller;
+
+import com.movieflix.controller.mapper.CategoryMapper;
+import com.movieflix.controller.request.CategoryRequest;
+import com.movieflix.controller.response.CategoryResponse;
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("movieflix/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping()
+    public ResponseEntity<List<CategoryResponse> >getAllCategories() {
+        return ResponseEntity.ok(categoryService.findAll()
+                .stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList());
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request){
+        Category category = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.createCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(savedCategory));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> findCategoryById(@PathVariable Long id) {
+        return categoryService.findCategoryById(id)
+                .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deteleCategoryById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
